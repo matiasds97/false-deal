89d3d19132eb6ed735f9baec977e91f454e02a14
Matias <matiasdestefano97@gmail.com>
2025-12-02 14:56:41 -0300

    feat: Start building the protagonist house

---
+++ scripts/entities/Door.gd
@@ -0,0 +1,25 @@
+extends Node3D
+
+var open: bool = false
+
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta: float) -> void:
+	if Input.is_action_just_pressed("interact"):
+		if open:
+			_close()
+		else:
+			_open()
+
+func _open() -> void:
+	var tween = create_tween()
+	tween.tween_property(self, "rotation_degrees", Vector3(0, -120, 0), 3.0)
+	open = true
+
+func _close() -> void:
+	var tween = create_tween()
+	tween.tween_property(self, "rotation_degrees", Vector3.ZERO, 3.0)
+	open = false
