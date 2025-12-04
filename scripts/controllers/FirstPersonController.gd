af893949aa2afaccbdc5b912a4b16a8628f9fd0c
Matias <matiasdestefano97@gmail.com>
2025-12-04 12:58:52 -0300

    feat: Implement FirstPersonController

---
+++ scripts/controllers/FirstPersonController.gd
@@ -0,0 +1,43 @@
+extends CharacterBody3D
+
+@export var speed = 5.0
+@export var mouse_sensitivity = 0.003
+@export var controller_sensitivity = 2.0
+
+@onready var head = $Head
+@onready var camera = $Head/Camera3D
+
+func _ready():
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
+func _unhandled_input(event):
+	if event is InputEventMouseMotion and Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
+		head.rotate_y(-event.relative.x * mouse_sensitivity)
+		camera.rotate_x(-event.relative.y * mouse_sensitivity)
+		camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-90), deg_to_rad(90))
+	
+	if event.is_action_pressed("ui_cancel"):
+		if Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
+			Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+		else:
+			Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
+func _physics_process(delta):
+	# Get the input direction and handle the movement/deceleration.
+	var input_dir = Input.get_vector("move_left", "move_right", "move_forward", "move_backward")
+	var direction = (head.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	if direction:
+		velocity.x = direction.x * speed
+		velocity.z = direction.z * speed
+	else:
+		velocity.x = move_toward(velocity.x, 0, speed)
+		velocity.z = move_toward(velocity.z, 0, speed)
+	
+	# Controller Look
+	var look_dir = Input.get_vector("look_left", "look_right", "look_up", "look_down")
+	if look_dir.length() > 0:
+		head.rotate_y(-look_dir.x * controller_sensitivity * delta)
+		camera.rotate_x(-look_dir.y * controller_sensitivity * delta)
+		camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-90), deg_to_rad(90))
+
+	move_and_slide()
